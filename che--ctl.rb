74e7998862274f559f868da415b54f7482280147
AdamKarsenti <51918191+AdamKarsenti@users.noreply.github.com>
2019-06-24 11:53:36 +0200

    Add files via upload

---
+++ che--ctl.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Chectl < Formula
+  desc "CLI to manage Eclipse Che server and workspaces"
+  homepage ""
+  url "https://github.com/che-incubator/chectl/releases/download/20190617213821/chectl-macos"
+  sha256 "78319c8a6045cbbd17a41e8533e8fb0813f2ee99223da028a9fa450e8d7c0f9b"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test chectl`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
